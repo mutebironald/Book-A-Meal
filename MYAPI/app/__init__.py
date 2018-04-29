05b6bfa27f1310b9d9e52c63ed3b47bf48a132c1
mutebironald4 <mutebironaldroninho@gmail.com>
2018-04-29 16:45:05 +0300

    changes app creation method

--- MYAPI/app/__init__.py
+++ MYAPI/app/__init__.py
@@ -2,13 +2,9 @@ from flask_api import FlaskAPI
 
 from flask import request, jsonify, abort
 
+app = FlaskAPI(__name__, instance_relative_config=True)
+app.config.from_object("config")
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-
-def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object("config")
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    
-    return app
 
