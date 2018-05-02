5d2f51125001b653327e35cc52d48dfb8817b3ef
mutebironald4 <mutebironaldroninho@gmail.com>
2018-05-02 16:02:51 +0300

    changes to structure

--- API/mockdbhelper.py
+++ API/mockdbhelper.py
@@ -2,15 +2,17 @@ import datetime
 
 
 MOCK_USERS = [
-    {"email": "ronald@example.com",
-     "salt": "8Fb23mMNHD5Zb8pr2qWA3PE9bH0=",
-     "hashed": "1736f83698df3f8153c1fbd6ce2840f8aace4f200771a46672635374073cc876cf0aa6a31f780e576578f791b5555b50df46303f0c3a7f2d21f91aa1429ac22e"
+    {
+    "email": "ronald@gmail.com",
+    "salt": "8Fb23mMNHD5Zb8pr2qWA3PE9bH0=",
+    "hashed": "1736f83698df3f8153c1fbd6ce2840f8aace4f200771a46672635374073cc876cf0aa6a31f780e576578f791b5555b50df46303f0c3a7f2d21f91aa1429ac22e"
      }]
 
 MOCK_MEALS = [{
     '_id': 1,
     'meal_name': 'Beef with Rice',
-    'owner':'ronald@example.com'
+    'price': 3000,
+    'owner':'ronald@gmail.com'
     }]
 
 MOCK_ORDERS = [{
@@ -24,6 +26,7 @@ MOCK_ORDERS = [{
 MOCK_MENUS = [{
      "_id": 1,
      "meal_name": "Beef with Rice", 
+     'price': 3000,
      "meal_id": "1"
      }]   
 
@@ -34,6 +37,7 @@ class MockDBHelper:
     def get_user(self, email):
         """Helps am already registered user to log in."""
         available_user = [user for user in MOCK_USERS if user["email"] == email]
+        print(available_user)
         if available_user:
             return available_user[0]
         return None
