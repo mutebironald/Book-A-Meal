2933c1d3357b3a12daec0734c36717f9beed777c
mutebironald4 <mutebironaldroninho@gmail.com>
2018-06-02 16:09:06 +0300

    Revert "Revert "changing from form to json format""
    This reverts commit dd5b308b41f98cc54390a09c5aed8bf9460d50b6.

--- api/bookameal.py
+++ api/bookameal.py
@@ -1,11 +1,16 @@
 from flask import  request, jsonify, make_response, json
 from flask_login import  login_user, logout_user, current_user
-from .classes.mockdbhelper import MOCK_USERS, MOCK_MEALS, MOCK_ORDERS, MOCK_MENUS
+from .classes.mockdbhelper import MOCK_USERS, MOCK_MEALS, MOCK_ORDERS, new_menu
 from .classes.user import User
 from . import app, PH, DB, basic_auth
 from . import login_manager
 import datetime
-from itsdangerous import URLSafeTimedSerializer
+import re
+# from itsdangerous import URLSafeTimedSerializer
+from flasgger import Swagger
+
+Swagger(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,37 +18,95 @@ def load_user(user_id):
     if user_password:
         return User(user_id)
 
+
+# from flask_login import current_user
+# from flask import url_for, redirect
+# from functools import wraps
+# def requires_roles(*roles):
+#     def wrapper(f):
+#         @wraps(f)
+#         def wrapped(*args, **kwargs):
+#             if current_user.get_role() not in roles:
+#                 return make_response('unauthorized', 401)
+#             return f(*args, **kwargs)
+#         return wrapped
+#     return wrapper
+# @requires_roles('admin')
+
+
 @app.route('/')
 def home():
-    """Starting point for the API"""
+    """
+    This is the language awesomeness API
+    Call this api passing a language name and get back its features
+    ---
+    tags:
+      - Awesomeness Language API
+    parameters:
+      - name: language
+        in: path
+        type: string
+        required: true
+        description: The language name
+      - name: size
+        in: query
+        type: integer
+        description: size of awesomeness
+    responses:
+      500:
+        description: Error The language is not awesome!
+      200:
+        description: A language with its awesomeness
+        schema:
+          id: awesome
+          properties:
+            language:
+              type: string
+              description: The language name
+              default: Lua
+            features:
+              type: array
+              description: The awesomeness list
+              items:
+                type: string
+              default: ["perfect", "simple", "lovely"]
+
+    """
+    #if not current_user.is_admin():
+        #return 'sorry'
+    #else:
     return "Welcome to Book-A-Meal"
 
 @app.route('/api/v1/auth/signup', methods=["POST"])
 def register():
     """Facilitates user registration"""
-    email = request.form.get('email')
-    if email:
-        password = request.form.get('password')
-        if password:
-            if DB.get_user(email):
-                return make_response("The email already exists", 409)
-            salt = PH.get_salt()
-            hashed = PH.get_hash(password + str(salt))
-            DB.add_user(email, salt, hashed)
-            return make_response("You are now registered", 201)
-        return make_response("You must enter a password", 400)
-    return make_response("Your email field is empty", 400)
+    data = request.get_json()
+    email = data['email']
+    if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+$)", email):
+        return jsonify({"error": "Invalid email."})
+    password = data['password']
+    if password.strip() == "":
+        return make_response("you must enter a password", 400)
+    if len(password) < 5:
+        return jsonify({"message": "Password too short"})
+    if DB.get_user(email):
+        return make_response("The email already exists", 409)
+    salt = PH.get_salt()
+    hashed = PH.get_hash(str(password) + str(salt))
+    DB.add_user(email, salt, hashed)
+    return make_response("You are now registered", 201)
     
 @app.route('/api/v1/auth/login', methods=['POST'])
 def login():
     """Facilitates user registration."""
-    email = request.form.get('email')
+    data = request.get_json()
+    email = data['email']
     if email:
-        password = request.form.get('password')
+        password = data['password']
         if password:
-            stored_user = DB.get_user(email) 
+            stored_user = DB.get_user(email)
             if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
-                user = User(email) 
+                user = User(email)
                 login_user(user)
                 return make_response("success!!, you are now logged in", 200)
             return make_response("Your email does not exist", 401)
@@ -61,8 +124,9 @@ def account_get_meals():
 @basic_auth.required
 def account_create_meal():
     """Enables Authenticated user to create meals"""
-    meal_name = request.form.get('meal_name')
-    price = request.form.get('price')
+    data = request.get_json()
+    meal_name = data['meal_name']
+    price = data['price']
     if meal_name:
         meal_id = DB.add_meal(meal_name, price, current_user.get_id())
         DB.update_meal(meal_id, meal_name, price)
@@ -75,8 +139,9 @@ def account_create_meal():
 @basic_auth.required
 def account_update_meal(meal_id):
     """Authenticated user is able to update meal"""
-    meal_name = request.form.get('meal_name')
-    price = request.form.get('price')
+    data = request.get_json()
+    meal_name = data['meal_name']
+    price = data['price']
     if meal_name:
         DB.update_meal(meal_id, meal_name, price)
         return jsonify({'meals': MOCK_MEALS}), 200
@@ -88,11 +153,12 @@ def account_update_meal(meal_id):
 @basic_auth.required
 def account_delete_meal(meal_id):
     """Authenticated user is able to delete particular meal"""
-    meal_id = request.form.get('meal_id')
     DB.delete_meal(meal_id)
     return make_response("The meal has been deleted", 202)
+    #else:
+        #return make_response("The meal is not present")
 
-@app.route('/api/v1/orders/<int:meal_id>', methods=['POST'])
+@app.route('/api/v1/orders', methods=['POST'])
 def new_order(meal_id):
     """Enables customer to make an order"""
     DB.add_order(meal_id, datetime.datetime.utcnow())
@@ -121,16 +187,18 @@ def remove_order(order_id):
 @app.route('/api/v1/menu')
 def get_menu():
     """Returns the menu"""
-    return jsonify({"MENU": MOCK_MENUS }), 200
+    return jsonify({"MENU":new_menu }), 200
     
 @app.route('/api/v1/menu', methods=["post"])
 @basic_auth.required
 def setup_menu():
     """Enables caterer to setup menu"""
-    meal_name = request.form.get('meal_name')
-    meal_id = request.form.get('meal_id')
-    DB.setup_menu(meal_id, meal_name)
-    return jsonify({"MENU": MOCK_MENUS}), 201
+    data =request.get_json()
+    meal_name = data['meal_name']
+    day = data['day']
+    price = data['price']
+    DB.setup_menu(meal_name, day, price)
+    return jsonify({"MENU": new_menu}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 from api import app
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
