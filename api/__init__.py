cc72f3aa386233dcdab12cb715ab99bf46a525d9
mutebironald4 <mutebironaldroninho@gmail.com>
2018-06-07 06:32:48 +0300

    removes flask login and mock db

--- api/__init__.py
+++ api/__init__.py
@@ -2,8 +2,7 @@ import os, binascii
 from flask import Flask
 from flask_basicauth import BasicAuth
 from .classes.passwordhelper import PasswordHelper
-# from .classes.mockdbhelper import MockDBHelper as DBHelper
-from flask_login import LoginManager
+# from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = binascii.hexlify(os.urandom(24))
@@ -12,9 +11,9 @@ app.config['BASIC_AUTH_USERNAME'] = 'Ronald'
 app.config['BASIC_AUTH_PASSWORD'] = 'Mutebi'
 
 basic_auth = BasicAuth(app)
-login_manager = LoginManager(app)
+# login_manager = LoginManager(app)
 
 PH = PasswordHelper()
-# DB = DBHelper()
+
 
 from . import bookameal
