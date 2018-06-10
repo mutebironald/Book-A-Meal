04d699b177c898235b15471483af0a099e0a3a8e
mutebironald4 <mutebironaldroninho@gmail.com>
2018-06-10 16:45:12 +0300

    removes basic auth

--- api/__init__.py
+++ api/__init__.py
@@ -6,10 +6,6 @@ from .classes.passwordhelper import PasswordHelper
 app = Flask(__name__)
 app.secret_key = binascii.hexlify(os.urandom(24))
 
-app.config['BASIC_AUTH_USERNAME'] = 'Ronald'
-app.config['BASIC_AUTH_PASSWORD'] = 'Mutebi'
-
-basic_auth = BasicAuth(app)
 PH = PasswordHelper()
 
 from . import controller
