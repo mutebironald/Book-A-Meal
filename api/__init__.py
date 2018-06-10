116b8085152f97ff4a78d6b05b7499120e08e8e4
mutebironald4 <mutebironaldroninho@gmail.com>
2018-06-10 22:56:35 +0300

    removes basic auth

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,10 @@
 import os, binascii
 from flask import Flask
-from flask_basicauth import BasicAuth
 from .classes.passwordhelper import PasswordHelper
 
 app = Flask(__name__)
 app.secret_key = binascii.hexlify(os.urandom(24))
+#app.secret_key = 'hahaha_not_hackable'
 
 PH = PasswordHelper()
 
