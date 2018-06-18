7fd8b2be0f0a980fd3eb22c798880e3e5bcb06f4
Ronald <36371394+mutebironald4@users.noreply.github.com>
2018-06-18 17:50:46 +0300

    changes tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -34,9 +34,9 @@ class TestMeals(unittest.TestCase):
         token = data['token']
         response = self.client.post('/api/v1/meals', data=json.dumps(meal), content_type='application/json',  headers={'Authorization': token})
         self.assertEqual(response.status_code, 200)
-        # response = self.client.post('/api/v1/menu', content_type = "application/json", data=json.dumps(content),  headers={
-        #         'Authorization': token})
-        # self.assertEqual(response.status_code, 201)
+        response = self.client.post('/api/v1/menu', content_type = "application/json", data=json.dumps(content),  headers={
+                'Authorization': token})
+        self.assertEqual(response.status_code, 201)
         # response = self.client.get('/api/v1/menu' , headers={'Authorization': token})
         # self.assertEqual(response.status_code, 200)
         
