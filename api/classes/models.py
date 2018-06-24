4487b44a84f36db3730cbb8993c44b864ea41327
Ronald <36371394+mutebironald4@users.noreply.github.com>
2018-06-24 14:05:25 +0300

    add logic from controller

--- api/classes/models.py
+++ api/classes/models.py
@@ -1,47 +1,72 @@
 import datetime
+import re
+from flask import make_response, jsonify
 
-ACCESS = {
-    'guest': 0,
-    'user': 1,
-    'admin': 2
-}
+from api import PH
+from ..authentication import Token
 
-class Users:
+
+class User:
     """A class for all users"""
-    
-    def __init__(self, access=ACCESS['user']):
+
+    def __init__(self):
+        """Initialises the user class"""
         self.users = []
         self.id = 1
-        self.access = access
 
     def get_user(self, email):
-        """Helps am already registered user to log in."""
+        """Retrieves a user from a list of users"""
         available_user = [user for user in self.users if user["email"] == email]
         if available_user:
             return available_user[0]
-        return None
+        return False
+
+    def verify_login(self,email, password):
+        """validates login details"""
+        response_message = "You are now logged in"
+        response_code = 200
+        absent_code = 401
+        stored_user = self.get_user(email)
+        if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
+            access_token = Token().generate_token(stored_user['id'])
+            return make_response(jsonify({"token": access_token, "message":response_message}), response_code)
+        return make_response("Please register then login", absent_code)
+
+    @staticmethod
+    def validate_email_and_password(email, password):
+        """Ensures signup with correct credentials"""
+        if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+$)", email):
+            return False
+        if password.strip() == "":
+            return False
+        if len(password) < 5:
+            return False
+        return True
 
     def add_user(self, email, salt, hashed):
-        """Enables user registration or signup"""
-        self.users.append({
-            "id": self.id , 
-            "email" : email, 
-            "salt": salt,
-            "hashed":hashed
-            })
+        """Helps add a user to the list of users"""
+        if self.get_user(email):
+            return False
+        new_user = { "id": self.id, "email": email, "salt": salt, "hashed": hashed }
+        self.users.append(new_user)
         self.id += 1
-        
-    def is_admin(self):
-        return self.access == ACCESS['admin']
+        return new_user
 
-    def allowed(self, access_level):
-        return self.access >= access_level
-
-class Meals:
+class Meal:
     """A class to represent the meals"""
+
     def __init__(self):
+        """Initialises the meal class"""
         self.meals = []
         self.id = 1
+    
+    def account_update_meal(self, meal_id, meal_name, price):
+        if isinstance(price, int) and meal_name:
+            self.update_meal(meal_id, meal_name, price)
+            update = self.get_meal(meal_id)
+            return jsonify({'meal': update}), 200
+        else:
+            return make_response("Enter a valid meal name and price", 400)
 
     def add_meal(self, meal_name, price):
         """Enables caterer to add a meal option"""
@@ -49,10 +74,18 @@ class Meals:
             "id": self.id,
             'meal_name': meal_name,
             'price': price
-            })
+        })
         self.id += 1
         return meal_name
-    
+
+    def create_meal(self, meal_name, price):
+        """Enables meal creation"""
+        if isinstance(price, int) and meal_name:
+            self.add_meal(meal_name, price)
+            return make_response("You successfully created a meal", 200)
+        else:
+            return make_response("Enter a valid meal name and price", 400)
+
     def update_meal(self, _id, meal_name, price):
         """Enables caterer to change a specific meal option"""
         for meal in self.meals:
@@ -60,7 +93,7 @@ class Meals:
                 meal['meal_name'] = meal_name
                 meal['price'] = price
                 break
-    
+
     def get_meals(self):
         """Returns all available meal options"""
         return self.meals
@@ -70,9 +103,9 @@ class Meals:
         for meal in self.meals:
             if meal['id'] == meal_id:
                 return meal
-            
-    def delete_meal( self, meal_id):
-        """Enables the caterer to resolve/remove orders"""
+
+    def delete_meal(self, meal_id):
+        """Enables the caterer to resolve/remove meal options"""
         c = 0
         for meal in self.meals:
             if meal['id'] == meal_id:
@@ -80,32 +113,63 @@ class Meals:
                 return True
             c += 1
 
+    def account_delete_meal(self, meal_id):
+        """Enables meal deletion"""
+        meal = self.delete_meal(meal_id)
+        if meal:
+          return make_response("The meal has been deleted", 202)
+        else:
+          return make_response("The meal specified is not present", 400)
+
+
 class Menu:
     """A class to represent the Menu of meals for a particular day"""
+
     def __init__(self, meals):
+        """Initialises the Menu class"""
         self.menu = []
         self.meals = meals
 
     def get_menu(self):
+        """Enables menu retrieval"""
         return self.menu
+
+    def account_get_menu(self):
+        menu = self.get_menu()
+        if menu:
+            return jsonify({"MENU":menu }), 200
+        else:
+            return make_response("The menu has not yet been set"), 404
+
+    def account_setup_menu(self, meal_id):
+        """Helps caterer to set the menu for a specific day"""
+        output = self.setup_menu(meal_id)
+        if output:
+          return jsonify({"MENU": output}), 201
+        else:
+          return make_response("Incorrect meal option"), 404
 
     def setup_menu(self, meal_id):
         meal_get = self.meals.get_meals()
         for meal in meal_get:
-            print (meal_get)
             if meal['id'] == int(meal_id):
-                print(meal)
                 self.menu.append(meal)
                 return self.menu
             else:
                 return False
 
-class Orders:
+class Order:
     """A class to represent a customers/users orders"""
+
     def __init__(self, menu):
+        """Initializes the order class"""
         self.orders = []
         self.menu = menu
         self.id = 1
+
+    def new_order(self, meal_id):
+        self.add_order(meal_id, datetime.datetime.utcnow())
+        return "Your order has been logged and a you will be served shortly"
 
     def add_order(self, meal_id, time):
         """Enables customer to make an order."""
@@ -121,7 +185,17 @@ class Orders:
                 self.id += 1
                 print(self.id)
                 return self.orders
-            
+
+    def get_all_orders(self):
+        """Enables caterer to retrieve all orders"""
+        now = datetime.datetime.utcnow()
+        orders = self.get_orders()
+        for order in orders:
+          deltaseconds = (now - order['time']).seconds
+          order['wait_minutes'] = "{}.{}".format((deltaseconds/60),
+                str(deltaseconds % 60).zfill(2))
+          return jsonify({"orders": orders}), 200
+
     def get_orders(self):
         """Returns all orders belonging to a particular caterer"""
         return self.orders
@@ -131,7 +205,7 @@ class Orders:
             if order['id'] == order_id:
                 return order
 
-    def delete_order( self, order_id):
+    def delete_order(self, order_id):
         """Enables the caterer to resolve/remove orders"""
         c = 0
         for order in self.orders:
