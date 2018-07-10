6e0ec080ea5dd745a754bbbf3e3700a754f36b60
Ronald <36371394+mutebironald4@users.noreply.github.com>
2018-07-10 08:48:08 +0300

    change controller to view

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,10 @@
 import os
 import binascii
 from flask import Flask
-from .classes.passwordhelper import PasswordHelper
+from .models.passwordhelper import PasswordHelper
 
 app = Flask(__name__)
 app.secret_key = binascii.hexlify(os.urandom(24))
 PH = PasswordHelper()
 
-from . import controller
+from . import views
