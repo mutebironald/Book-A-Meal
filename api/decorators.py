a39283fe79d2c9a8f3b1d2080c429216ea2ea6ee
Ronald <36371394+mutebironald4@users.noreply.github.com>
2018-07-10 08:50:57 +0300

    Add decorator

---
+++ api/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        access_token = request.headers.get("Authorization")
+        if access_token:
+            try:
+                user_id = auth.decode_token(access_token)
+
+            except Exception as e:
+                return str(e)
+
+            else:
+                if user_id and isinstance(user_id, int):
+                    return f(*args, **kwargs)
+                else:
+                    return jsonify({"message": "Invalid token"}), 401
+        return jsonify({"message": "Token is missing"}), 400
+    return wrap
+    
